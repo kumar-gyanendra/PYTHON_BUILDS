7e403184bd8162c5fc6eb9f653dddc2501225b0e
kumar-gyanendra <krgyanendra213@gmail.com>
2025-11-28 00:38:21 +0530

    Added Problem_09.py (Easy)

---
+++ Problem_09.py
@@ -0,0 +1,9 @@
+'''
+9. Write a program to format the following letter using escape sequence 
+characters. 
+letter = "Dear Harry, this python course is nice. Thanks!"
+'''
+
+#Solution
+Letter = "Dear Harry,\n\tThis python course is nice.\nThanks!"    
+print(Letter)
